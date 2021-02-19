80d038797476e550eb251dad41840d88eb70a87a
Alexsandro-Assun <77842348+Alexsandro-Assun@users.noreply.github.com>
2021-02-19 15:02:29 -0300

    Add files via upload

---
+++ educaUS/src/main/java/com/educaUS/educaUS/EducaUsApplication.java
@@ -0,0 +1,13 @@
+package com.educaUS.educaUS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducaUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducaUsApplication.class, args);
+	}
+
+}
---
+++ educaUS/src/test/java/com/educaUS/educaUS/EducaUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educaUS.educaUS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducaUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
